4d67019a6e77625d229bf38911e8d00ed26344e9
Sreehari Prathap <sreehariprathap1996@gmail.com>
2022-10-12 23:02:34 +0530

    implemented simple validations using class validators

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -10,7 +11,7 @@ export class AuthService {
     return { message: 'I have signed in' };
   }
 
-  signUp(params) {
-    return params
+  signUp(dto:AuthDto) {
+    return dto
   }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class AuthDto {
-//   @IsEmail()
-//   @IsNotEmpty()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
