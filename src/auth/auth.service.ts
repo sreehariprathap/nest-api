b999c9c50a189079d371c8e6e4860a6e283b201c
Sreehari Prathap <sreehariprathap1996@gmail.com>
2022-10-13 12:55:56 +0530

    relations and custom error messages

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ForbiddenException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class AuthService {
@@ -15,17 +17,29 @@ export class AuthService {
   async signUp(dto: AuthDto) {
     // generate the password
     const hash = await argon.hash(dto.password);
-    // save the new user in deb
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-        firstName:dto.firstName,
-        lastName:dto.lastName
-      },
-    });
-    // return the saved user
-    return user;
+    // save the new user in db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          firstName:dto.firstName,
+          lastName:dto.lastName
+        },
+      });
+      delete user.hash; // remove the hash from the user data
+      // return the saved user
+      return user;
+    } catch (error) {
+      if(error instanceof PrismaClientKnownRequestError){
+        if(error.code === 'P2002'){
+          throw new ForbiddenException(
+            'Credentials already taken'
+          )
+        }
+      }
+      throw error;
+    }
   }
 
   async getUsers(){
