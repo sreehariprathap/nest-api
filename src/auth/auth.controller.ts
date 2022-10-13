c08ef85bbfaf431142dc3a1be5891effafa28445
Sreehari Prathap <sreehariprathap1996@gmail.com>
2022-10-13 13:05:32 +0530

    singin logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,8 +15,8 @@ export class AuthController {
   }
 
   @Post('signin')
-  signIn() {
-    return this.authService.signIn();
+  signIn(@Body() dto:AuthDto) {
+    return this.authService.signIn(dto);
   }
 
   @Get('users')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,10 +10,6 @@ import { ForbiddenException } from '@nestjs/common/exceptions';
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signIn() {
-    return { message: 'I have signed in' };
-  }
-
   async signUp(dto: AuthDto) {
     // generate the password
     const hash = await argon.hash(dto.password);
@@ -23,26 +19,42 @@ export class AuthService {
         data: {
           email: dto.email,
           hash,
-          firstName:dto.firstName,
-          lastName:dto.lastName
+          firstName: dto.firstName,
+          lastName: dto.lastName,
         },
       });
       delete user.hash; // remove the hash from the user data
       // return the saved user
       return user;
     } catch (error) {
-      if(error instanceof PrismaClientKnownRequestError){
-        if(error.code === 'P2002'){
-          throw new ForbiddenException(
-            'Credentials already taken'
-          )
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already taken');
         }
       }
       throw error;
     }
   }
 
-  async getUsers(){
+  async signIn(dto: AuthDto) {
+    // find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('User does not exist');
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if(!pwMatches) throw new ForbiddenException('Password incorrect');
+    // send back the user if successfull
+    delete user.hash;
+    return user;
+  }
+
+  async getUsers() {
     const users = await this.prisma.user.findMany();
     return users;
   }
